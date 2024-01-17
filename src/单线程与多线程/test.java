4a1314997f3c948decb0608997f280a2f1bd82c2
Sky_Interest <841102500@qq.com>
2024-01-17 10:22:13 +0800

    提交

---
+++ src/单线程与多线程/test.java
@@ -0,0 +1,12 @@
+package 单线程与多线程;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+
+    }
+}
